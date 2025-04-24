f371a87f5453dd7429a1ca1c1b713679ec5b1027
Codagis <codagistech@gmail.com>
2025-04-24 17:27:30 -0300

    (hotfix):retirando import desnecessário

--- src/main/java/com/ordem/servico/api/repository/ImageRepository.java
+++ src/main/java/com/ordem/servico/api/repository/ImageRepository.java
@@ -4,8 +4,6 @@ import com.ordem.servico.api.model.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
 
